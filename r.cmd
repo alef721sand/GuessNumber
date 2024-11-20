373bb0072f28f967b4451701f7660f7e06a9dcb0
Alexander Efimtsev <alef721@gmail.com>
2024-11-20 12:12:38 +0300

    remove extra file

--- r.cmd
+++
@@ -1,7 +0,0 @@
-rem echo "# GuessNumber" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/alef721sand/GuessNumber.git
-git push -u origin main
